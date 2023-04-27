d251a70389827f837ead7b4f493053aef69f4f56
Raj Rai <rrai1590@gmail.com>
2023-04-27 12:03:19 +0530

    temp

---
+++ app.js
@@ -0,0 +1,89 @@
+{
+  "original_content": "DG ISPR Maj General Ahmed Sharif negated the claims about the Pak army’s ‘operational preparedness’, threatening to take the ‘battle to India’s territory’ if the situation arises.General Sharif claimed that Pakistan’s Air Force during Operation Swift Retort showed the “resilience of nation and resolve of Air Force.“Forty Central Reserve Police Force (CRPF) personnel were killed in the terrorist attack on a convoy of security vehicles in Jammu and Kashmir’s Pulwama on February 14, 2019. As a response to the Pulwama terror attack, 12 Indian Air Force Mirage-2000 fighter jets struck on Jaish-e-Mohammed training camp in Balakot inside Pakistan in the early hours on February 26 with precision-guided munitions.The entire operation of launching the airstrikes and coming back to Indian soil safely was codenamed ‘Operation Bandar.’ The name was given to maintain secrecy and ensure that the plans of the airstrikes don’t leak out.",
+  "processed_content": [
+    {
+      "prompt": "Please summarize the main points from the given content in 4-5 bullet points. Content: {input}",
+      "model_info": {
+        "model": "text-davinci-002",
+        "prompt": "prompt",
+        "temperature": 0.6,
+        "max_tokens": 1000
+      },
+      "generated_solution": [
+        "-DG ISPR Maj General Ahmed Sharif negated the claims about the Pak army’s ‘operational preparedness’, threatening to take the ‘battle to India’s territory’ if the situation arises.",
+        "-General Sharif claimed that Pakistan’s Air Force during Operation Swift Retort showed the “resilience of nation and resolve of Air Force.",
+        "-As a response to the Pulwama terror attack, 12 Indian Air Force Mirage-2000 fighter jets struck on Jaish-e-Mohammed training camp in Balakot inside Pakistan in the early hours on February 26 with precision-guided munitions.",
+        "-The entire operation of launching the airstrikes and coming back to Indian soil safely was codenamed ‘Operation Bandar.’ The name was given to maintain secrecy and ensure that the plans of the airstrikes don’t leak out."
+      ]
+    },
+    {
+      "prompt": "Please summarize the main points from the given content in 4-5 bullet points. Content: {input}",
+      "model_info": {
+        "model": "text-davinci-003",
+        "prompt": "prompt",
+        "temperature": 0.6,
+        "max_tokens": 1000
+      },
+      "generated_solution": [
+        "- DG ISPR Maj General Ahmed Sharif negated claims about Pak army’s ‘operational preparedness’ and threatened to take the ‘battle to India’s territory’ if the situation arises. ",
+        "- Pakistan’s Air Force showed “resilience of nation and resolve of Air Force” during Operation Swift Retort in response to the Pulwama terror attack.",
+        "- 12 Indian Air Force Mirage-2000 fighter jets struck on Jaish-e-Mohammed training camp in Balakot inside Pakistan in the early hours on February 26.",
+        "- The entire operation of launching the airstrikes and coming back to Indian soil safely was codenamed ‘Operation Bandar’ to maintain secrecy and ensure that the plans of the airstrikes don’t leak out."
+      ]
+    },
+    {
+      "prompt": "Please summarize the main points from the given content in 4-5 bullet points. Content: {input}",
+      "model_info": {
+        "model": "text-davinci-001",
+        "prompt": "prompt",
+        "temperature": 0.6,
+        "max_tokens": 1000
+      },
+      "generated_solution": [
+        "-Maj General Ahmed Sharif, DG ISPR, negated the claims about the Pak army’s ‘operational preparedness’, threatening to take the ‘battle to India’s territory’ if the situation arises.",
+        "-Pakistan’s Air Force during Operation Swift Retort showed the “resilience of nation and resolve of Air Force.“",
+        "-Forty Central Reserve Police Force (CRPF) personnel were killed in the terrorist attack on a convoy of security vehicles in Jammu and Kashmir’s Pulwama on February 14, 2019.",
+        "-As a response to the Pulwama terror attack, 12 Indian Air Force Mirage-2000 fighter jets struck on Jaish-e-Mohammed training camp in Balakot inside Pakistan in the early hours on February 26 with precision-guided munitions.",
+        "-The entire operation of launching the airstrikes and coming back to Indian soil safely was codenamed ‘Operation Bandar.’ The name was given to maintain secrecy and ensure that the plans of the airstrikes don’t leak out."
+      ]
+    },
+    {
+      "prompt": "Please summarize the main points from the given content in 4-5 bullet points. Content: {input}",
+      "model_info": {
+        "model": "text-babbage-001",
+        "prompt": "prompt",
+        "temperature": 0.6,
+        "max_tokens": 1000
+      },
+      "generated_solution": [
+        "- DG ISPR Maj General Ahmed Sharif negated the claims about the Pak army’s ‘operational preparedness’, threatening to take the ‘battle to India’s territory’ if the situation arises.",
+        "- General Sharif claimed that Pakistan’s Air Force during Operation Swift Retort showed the “resilience of nation and resolve of Air Force.”",
+        "- Forty Central Reserve Police Force (CRPF) personnel were killed in the terrorist attack on a convoy of security vehicles in Jammu and Kashmir’s Pulwama on February 14, 2019.",
+        "- As a response to the Pulwama terror attack, 12 Indian Air Force Mirage-2000 fighter jets struck on Jaish-e-Mohammed training camp in Balakot inside Pakistan in the early hours on February 26 with precision-guided munitions.",
+        "- The entire operation of launching the airstrikes and coming back to Indian soil safely was codenamed ‘Operation Bandar.’ The name was given to maintain secrecy and ensure that the plans of the airstrikes don’t leak out."
+      ]
+    },
+    {
+      "prompt": "Please summarize the main points from the given content in 4-5 bullet points. Content: {input}",
+      "model_info": {
+        "model": "text-curie-001",
+        "prompt": "prompt",
+        "temperature": 0.6,
+        "max_tokens": 1000
+      },
+      "generated_solution": [
+        "-Pakistan DG ISPR Maj General Ahmed Sharif negated the claims about the Pak army’s ‘operational preparedness’, threatening to take the ‘battle to India’s territory’ if the situation arises. ",
+        "-General Sharif claimed that Pakistan’s Air Force during Operation Swift Retort showed the “resilience of nation and resolve of Air Force.“ ",
+        "-Forty Central Reserve Police Force (CRPF) personnel were killed in the terrorist attack on a convoy of security vehicles in Jammu and Kashmir’s Pulwama on February 14, 2019. ",
+        "-As a response to the Pulwama terror attack, 12 Indian Air Force Mirage-2000 fighter jets struck on Jaish-e-Mohammed training camp in Balakot inside Pakistan in the early hours on February 26 with precision-guided munitions. ",
+        "-The entire operation of launching the airstrikes and coming back to Indian soil safely was codenamed ‘Operation Bandar.’ The name was given to maintain secrecy and ensure that the plans of the airstrikes don’t leak out."
+      ]
+    }
+  ],
+  "model_specification": {
+    "text-davinci-003": "text-davinci-003 is the most capable and versatile model, as it has been trained on the largest and most diverse set of data, and can perform a wide range of tasks, such as summarization, translation, and conversation. However, it is also the most computationally expensive and may have longer response times.",
+    "text-babbage-001": "text-babbage-001 is a smaller and more efficient model than text-davinci-003, and is best suited for simpler language tasks such as classification, sentiment analysis, and question-answering. It may also have faster response times than text-davinci-003",
+    "text-curie-001": "text-curie-001 is a model that strikes a balance between the other two models in terms of size and computational efficiency. It is designed to be more accurate than text-babbage-001 for complex language tasks, while still being faster and less resource-intensive than text-davinci-003"
+  }
+}
+
